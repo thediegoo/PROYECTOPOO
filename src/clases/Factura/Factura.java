b48e4b77d370f3e60caadecc330f9bb6d0fa514f
Ivan Atanacio Anchiraico <ivan.atanacio@gmail.com>
2022-05-15 13:29:11 -0500

    Factura 15052022
    Creacion de factura :+1: traer ayuda de cliente, producto, inserta a grilla , pero falta guardar lso datos de la factura.

--- src/clases/Factura/Factura.java
+++ src/clases/Factura/Factura.java
@@ -13,7 +13,7 @@ public class Factura {
     private Cliente cliente;
     private double total;
     private String regalo;
-
+    private static int cantidadregistros = 0; 
     public Factura(String codigo, Fecha fecha, Empleado vendedor, Cliente cliente, double total, String regalo) {
         this.codigo = codigo;
         this.fecha = fecha;
@@ -21,13 +21,16 @@ public class Factura {
         this.cliente = cliente;
         this.total = total;
         this.regalo = regalo;
+        cantidadregistros++;
     }
     
     public String imprimir(){
         String mensaje = "Codigo :"+codigo+"/nFecha :"+fecha+"/nEmpleado: "+vendedor+"/nCliente :"+cliente+"/nRegalo: "+regalo+"/nTotal :"+total;
         return mensaje;
     }
-
+    public Factura(){
+        cantidadregistros++;
+    }
     public String getRegalo() {
         return regalo;
     }
@@ -51,4 +54,15 @@ public class Factura {
     public double getTotal() {
         return total;
     }
+    
+    
+    public String generarSerie(){
+        return "FA001";
+    }
+    
+    public String generarCorrelativo(){
+        return String.format("%08d", cantidadregistros);
+        
+    }
+    
 }
